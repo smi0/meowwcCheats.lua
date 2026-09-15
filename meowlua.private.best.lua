494d024198b682338c6d335be5ef95f6dacef893
smi0 <hashmiramiebusiness@gmail.com>
2026-09-14 18:21:54 -0700

    Update meowlua.private.best.lua

--- meowlua.private.best.lua
+++ meowlua.private.best.lua
@@ -1,9 +1,7 @@
-
 repeat task.wait() until game:IsLoaded()
 
 if game.GameId ~= 6035872082 then
-    warn("Named Enhancements: RIVALS only")
     return
 end
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/8a5f2b868628c250a7686c1f03a1768b.lua"))()"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/
