d5a6e8f237430d0c4a0a75b1a08e28c22199e456
smi0 <hashmiramiebusiness@gmail.com>
2026-09-14 18:19:59 -0700

    Implement game ID validation in meowlua.private.best.lua
    Add game ID check for Named Enhancements: RIVALS.

--- meowlua.private.best.lua
+++ meowlua.private.best.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/8a5f2b868628c250a7686c1f03a1768b.lua"))()
+
+repeat task.wait() until game:IsLoaded()
+
+if game.GameId ~= 6035872082 then
+    warn("Named Enhancements: RIVALS only")
+    return
+end
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/8a5f2b868628c250a7686c1f03a1768b.lua"))()"))()
